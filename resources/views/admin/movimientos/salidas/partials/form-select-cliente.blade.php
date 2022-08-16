954ce3e2cf393b1e319a2ebda23ce9361401b3e6
SDEV - Ivan D. Fontana <ivan@sistemasdev.com>
2022-08-16 13:30:46 -0300

    avances

--- resources/views/admin/movimientos/salidas/partials/form-select-cliente.blade.php
+++ resources/views/admin/movimientos/salidas/partials/form-select-cliente.blade.php
@@ -27,12 +27,12 @@
                 </div>
 
                 @if(isset($depositos))
-                    <div class="col-md-2 @if(!isset($es_traslado_depositos) && !$es_traslado_depositos) not-display @endif" id="desde-deposito">
+                    <div class="col-md-2 @if(!isset($es_traslado_depositos)) not-display @endif" id="desde-deposito">
                         <label class="text-body">Desde depósito</label>
                         <select class="form-control bg-transparent" name="desde_deposito" id="desde_deposito">
                             <option value="">Seleccione Dep. Origen</option>
                             @foreach ($depositos as $deposito)
-                                <option value="{{$deposito->id}}" @if(isset($es_traslado_depositos) && $es_traslado_depositos && $deposito->id == $desde_deposito->id) selected @endif >
+                                <option value="{{$deposito->id}}" @if(isset($es_traslado_depositos) && $deposito->id == $desde_deposito->id) selected @endif >
                                     {{$deposito->razon_social}} - {{$deposito->description}}
                                 </option>
                             @endforeach
@@ -44,7 +44,7 @@
                         <select class="form-control bg-transparent" name="a_deposito" id="a_deposito">
                             <option value="">Seleccione Dep. Destino</option>
                             @foreach ($depositos as $deposito)
-                                <option value="{{$deposito->id}}" @if(isset($es_traslado_depositos) && $es_traslado_depositos && $deposito->id == $a_deposito->id) selected @endif >
+                                <option value="{{$deposito->id}}" @if(isset($es_traslado_depositos) && $deposito->id == $a_deposito->id) selected @endif >
                                     {{$deposito->razon_social}} - {{$deposito->description}}
                                 </option>
                             @endforeach
