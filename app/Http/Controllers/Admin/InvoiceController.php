a39a3728b37ed9d9ee60a3626578a9255160c70a
SDEV - Ivan D. Fontana <ivan@sistemasdev.com>
2022-08-22 10:19:11 -0300

    arreglos

--- app/Http/Controllers/Admin/InvoiceController.php
+++ app/Http/Controllers/Admin/InvoiceController.php
@@ -256,7 +256,7 @@ class InvoiceController extends Controller
                 }
 
                 if (!isset($movement->factura_flete) && $movement->flete > 0) {
-                    if(!Panamas::where('movement_id', $movement->id)->where('tipo','FLE')->orWhere('tipo','FLETE T')->orWhere('tipo','FLETE P')->exists()){
+                    if(!Panamas::where('movement_id', $movement->id)->where('tipo', 'LIKE', 'FLE%')->exists()){
                         $data_panama['tipo']               = 'FLE';
                         $data_panama['orden']              = $orden + 1;
                         $data_panama['neto105']            = 0.0;
